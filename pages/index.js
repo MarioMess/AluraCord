d7bac3889060a125fc0aca1f3920cce950d771ff
Mário Gonçalves da Silva <mariskovisk@gmail.com>
2022-01-25 14:44:36 -0300

    Third commit

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
 import appConfig from '../config.json';
+import {useRouter} from 'next/router';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: Roman;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -59,11 +33,12 @@ function Titulo(props) {
 //export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'MarioMess';
+    //const username = 'MarioMess';
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();    
 
     return (
-        <>
-            <GlobalStyle />
+        <>         
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -90,6 +65,11 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubimit={function (infosDoEvento) {
+                            infosDoEvento.prevenDefault();                            
+                            console.log('alguem está submetendo uma chamada')
+                            roteamento.push('/chat');                            
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -100,7 +80,19 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
 
+                        {/*<input type="text"value={username} onChange={function handler(event){
+                            console.log('usuario digitou')
+                            const valor  = event.target.value;
+                            setUsername(valor);
+                        }}
+                        />*/}
+
                         <TextField
+                            value={username} onChange={function handler(event) {
+                                console.log('usuario digitou')
+                                const valor = event.target.value;
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -113,7 +105,7 @@ export default function PaginaInicial() {
                         />
                         <Button
                             type='submit'
-                            label='Entrar'                            
+                            label='Entrar'
                             fullWidth
                             buttonColors={{
                                 contrastColor: appConfig.theme.colors.neutrals["000"],
